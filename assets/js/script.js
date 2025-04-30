029bbfbc410dbdb84197fffcf5c969c55a4fa353
Abdallayasseer <2302939@student.eelu.edu.eg>
2025-04-30 07:50:48 +0300

    Bakery Website

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@ const $logoImg = document.getElementById("logo-img");
 window.addEventListener("scroll", () => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
   $logoImg.src =
-    window.scrollY > 50 ? "../../bakery-color.png" : "../../logo.png";
+    window.scrollY > 50 ? "../../../bakery-color.png" : "../../../logo.png";
 });
